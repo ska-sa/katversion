9493d14b181bb1a8c383e497f42200fb300d3516
Ludwig Schwardt <ludwig@ska.ac.za>
2016-01-26 15:27:37 +0200

    Eat your own dog food

--- katversion/__init__.py
+++ katversion/__init__.py
@@ -1,5 +1,7 @@
 from .version import get_version, build_info
 from .build import setup
 
+# BEGIN VERSION CHECK
 # Get package version when locally imported from repo or via -e develop install
 __version__ = get_version(__path__[0])
+# END VERSION CHECK
