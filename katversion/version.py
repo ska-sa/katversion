25540523b5c992eb4aa2f0d17e3ce3d82b958dd7
Ludwig Schwardt <ludwig@ska.ac.za>
2016-02-09 12:15:31 +0200

    Fix bug when repo has no tags
    If the repo has no tags, 'git describe' only returns a commit hash and
    optionally a dirty marker. The code fleshed out the missing parts but used
    floats and ints instead of the expected strings, causing crashes. Ensure that
    a release version is not triggered without a proper tag.

--- katversion/version.py
+++ katversion/version.py
@@ -76,10 +76,9 @@ def get_git_version(path=None):
                        '--dirty', '--always')
     git_desc_parts = git_desc.strip().split('-')
 
-    if len(git_desc_parts) == 1:
-        git_desc_parts = [0.0, 0, git_desc_parts[0]]
-    elif len(git_desc_parts) == 2:
-        git_desc_parts = [0.0, 0] + git_desc_parts
+    # If repo contains no tags, start version off at 0.0 (but can't be release)
+    if len(git_desc_parts) < 3:
+        git_desc_parts = ['0.0', '1'] + git_desc_parts
 
     branch_name = run_cmd(path, 'git', 'rev-parse', '--abbrev-ref', 'HEAD')
     branch_name = branch_name.strip().lower()
